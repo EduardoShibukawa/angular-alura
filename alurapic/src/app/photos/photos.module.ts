deef6e44407b0674069f51aa160aaf5547c1a7b3
eduardoshibukawa <eduardoshibuka@gmail.com>
2021-04-17 15:09:45 -0300

    first commit

---
+++ alurapic/src/app/photos/photos.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotosListComponent } from './photos-list/photos-list.component';
+import { PhotosFormComponent } from './photos-form/photos-form.component';
+
+@NgModule({
+    declarations: [PhotoComponent, PhotosListComponent, PhotosFormComponent],
+    imports: [
+        CommonModule,
+        HttpClientModule
+    ]
+})
+export class PhotosModule { }
