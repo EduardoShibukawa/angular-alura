d6235237d0af89752d605002654e3bacd958f2e2
eduardoshibukawa <eduardoshibuka@gmail.com>
2021-05-09 17:02:20 -0300

    Finalizado primeiro curso de teste em angular

--- ng-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ ng-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -5,18 +5,44 @@ import { LikeWidgetModule } from './like-widget.module';
 
 describe(LikeWidgetComponent.name, () => {
     let fixture: ComponentFixture<LikeWidgetComponent> = null;
+    let component: LikeWidgetComponent = null;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [LikeWidgetModule]
         }).compileComponents();
-
+        
         fixture = TestBed.createComponent(LikeWidgetComponent);
+        component = fixture.componentInstance;
     });
 
     it('Should create component', () => {
-        const instance = fixture.componentInstance;
-
-        expect(instance).toBeTruthy();
+        expect(component).toBeTruthy();
     });
+
+    it('Should auto-generate ID during ngOnInit when (@Input id) is not assigned', () => {
+        fixture.detectChanges();
+
+        expect(component.id).toBeTruthy();
+    });
+
+    it('Should NOT auto-generate ID during ngOnInit when (@Input id) is assigned', () => {
+        const expectedId = 'someId';
+
+        component.id = expectedId;
+
+        fixture.detectChanges();
+
+        expect(component.id).toBe(expectedId);
+    });
+
+    it(`#${LikeWidgetComponent.prototype.like.name} 
+        should trigger (@Output liked) when called`, () => {
+        spyOn(component.liked, 'emit')
+        fixture.detectChanges();
+        
+        component.like();
+
+        expect(component.liked.emit).toHaveBeenCalled();
+    })
 })
